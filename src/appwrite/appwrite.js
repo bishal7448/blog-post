64fc7f9b55cd6ab771fb1a5dfe205c15d5630cd9
Bishal Saha <saha.bishal7676@gmail.com>
2026-03-06 10:49:34 +0530

    Prepare for Vercel deployment

--- src/appwrite/appwrite.js
+++ src/appwrite/appwrite.js
@@ -1,8 +1,9 @@
 import { Client, Account, Databases } from "appwrite";
+import configEnv from "../config/configEnv";
 
 const client = new Client()
-    .setEndpoint("https://fra.cloud.appwrite.io/v1")
-    .setProject("6948e5a90014745ad64b");
+    .setEndpoint(configEnv.appwriteUrl)
+    .setProject(configEnv.appwriteProjectId);
 
 const account = new Account(client);
 const databases = new Databases(client);
